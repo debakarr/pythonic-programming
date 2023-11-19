6c2018449bb2da95a0dcef1d70ce583fd1c67539
Roy, Debakar <debakar.roy@intel.com>
2023-11-19 09:16:07 +0530

    Updating all the lesson with html slides

---
+++ content/tmp.py
@@ -0,0 +1,14 @@
+def myFunction(x, y, z):
+    a = x + y
+    b = z
+    if x > y:
+        print(f"{x} is greater than {y}")
+    else:
+        print(f"{y} is greater than or equal to {x}")
+
+
+def square(my_number):
+    return mynumber**2
+
+
+print("Hello, World!")
